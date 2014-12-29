299e1af15135102694e9cd63cf14d49cdcb8d22c
lizzy <lizzy.999@qq.com>
2014-12-29 19:12:56 +0800

    主页

---
+++ application/controllers/index.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * 功能：主页控制器
+ * 作者：lizzyphy
+ * 日期：2014.12.29
+ */
+class Index extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+	}
+	
+	public function index()
+	{
+		$this->load->view('main_nav');
+	}
+}
