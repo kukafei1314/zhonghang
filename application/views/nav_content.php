a8b7fb942ccc9485312450ca2c6da4867ae02dfa
Jason <lisen9129@163.com>
2014-12-30 22:45:38 +0800

    Jason

---
+++ application/views/nav_content.php
@@ -0,0 +1,55 @@
+<div class="list_right">
+    	<div id="right_head">
+        	<p><a href="http://est.bit.edu.cn/index">首页</a>&nbsp;>&nbsp;<a href="">新闻中心</a>&nbsp;>&nbsp;<a href="">新闻快讯</a></p>
+        </div>
+        <div id="right_title">
+        	<p>新闻快讯</p>
+        </div>
+        <div id="right_content">
+            <ul>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=426">北京市安全生产科学技术研究院贾秋霞院长一行来重点实验室参观调研</a></span>
+                <span class="time">2014-09-22</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=425">空气动力学国家重点实验室王勋年主任一行来重点实验室调研</a></span>
+                <span class="time">2014-09-22</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=364">爆炸科学与技术国家重点实验室2014工作会议</a></span>
+                <span class="time">2014-04-04</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=363">科技后备人才早期培养计划师生见面会</a></span>
+                <span class="time">2014-03-17</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=347">爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议</a></span>
+                <span class="time">2013-09-02</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=328">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
+                <span class="time">2013-03-25</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=295">“爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开</a></span>
+                <span class="time">2012-09-03</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=292">重点实验室2012年工作会议召开</a></span>
+                <span class="time">2012-07-31</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=280">陈鹏万教授参加第十一届爆炸合成新材料国际研讨会</a></span>
+                <span class="time">2012-06-13</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=261">张建国、王丽琼教授参加捷克“第15届含能材料研究新趋势国际会议”</a></span>
+                <span class="time">2012-04-25</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=232">北京理工大学安全培训中心接受资质复审</a></span>
+                <span class="time">2011-11-02</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=213">爆炸学科与技术国家重点实验室召开发展研讨会</a></span>
+                <span class="time">2011-08-26</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=212">第三届全国强动载效应及防护学术会议顺利召开</a></span>
+                <span class="time">2011-08-23</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=211">爆炸科学与技术国家重点实验室第四届学术委员会第 6 次会议顺利召开</a></span>
+                <span class="time">2011-08-16</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=207">爆炸科学与技术国家重点实验室（北京理工大学）山东研究基地会议纪要</a></span>
+                <span class="time">2011-07-18</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=199">爆炸科学与技术国家重点实验室（北京理工大学）山东研究基地第二届理事会会议预备会在北理工顺利召开</a></span>
+                <span class="time">2011-06-21</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=185">张建国副教授参加2011年北京市科技新星入选人员交流大会</a></span>
+                <span class="time">2011-04-26</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=180">爆炸科学与技术国家重点实验室召开2011年工作会议</a></span>
+                <span class="time">2011-04-12</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=176">爆破技术中心协助北阳爆破成功拆除锦州港旧粮仓</a></span>
+                <span class="time">2011-03-28</span>                </li>
+                            <li><span class="content2"><img src="2ji_list.jpg" width="10" height="10" />&nbsp<a href="http://est.bit.edu.cn/article/?aid=155">中国力学学会第58次青年学术沙龙在我校成功举行</a></span>
+                <span class="time">2010-11-25</span>                </li>
+                        </ul>
+            <div id="page" style="text-align:center; font-size:14px;";>
+            	&nbsp;<strong>1</strong>&nbsp;<a href="http://est.bit.edu.cn/article/type/?type=74&amp;p=2">2</a>&nbsp;<a href="http://est.bit.edu.cn/article/type/?type=74&amp;p=3">3</a>&nbsp;<a href="http://est.bit.edu.cn/article/type/?type=74&amp;p=2">下一页</a>&nbsp;            </div>
+        </div>
+    </div>
+	<div class="clear"></div>
