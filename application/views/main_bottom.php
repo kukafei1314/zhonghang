bbbf9b490a66efba4d94583cd4308f2026ed58f4
lizzy <lizzy.999@qq.com>
2014-12-30 16:26:20 +0800

    bottom 1200body

--- application/views/main_bottom.php
+++ application/views/main_bottom.php
@@ -3,15 +3,104 @@
         	<div class="bottom_title">
             	公司概况
             </div>
+            <div class="bottom_na">
+            	<div class="bottom_nav_left">
+                	<ul>
+                    	<li><a href="#">公司简介</a></li>
+                        <li><a href="#">董事长致辞</a></li>
+                        <li><a href="#">管理团队</a></li>
+                        <li><a href="#">组织机构</a></li>
+                    </ul>
+                </div>
+                <div class="bottom_nav_left1">
+                	<ul>
+                    	<li><a href="#">公司战略</a></li>
+                        <li><a href="#">荣誉与奖励</a></li>
+                        <li><a href="#">企业文化</a></li>
+                    </ul>
+                </div>
+                <div class="cl"></div>
+            </div>
         </div>
-        <div class="bottom_nav1">
+        <div class="bottom_nav2">
+        	<div class="bottom_title">
+            	动态要闻
+            </div>
+            <div class="bottom_na">
+            	<div class="bottom_nav_left2">
+                	<ul>
+                    	<li><a href="#">公司动态</a></li>
+                        <li><a href="#">图片新闻</a></li>
+                        <li><a href="#">媒体聚焦</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="bottom_nav1">
+        <div class="bottom_nav3">
+        	<div class="bottom_title">
+            	业务总览
+            </div>
+            <div class="bottom_na">
+            	<div class="bottom_nav_left2">
+                	<ul>
+                    	<li><a href="#">业务领域</a></li>
+                        <li><a href="#">水务投资项目</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="bottom_title" style="padding-top:10px;">
+            	业界观察
+            </div>
+            <div class="bottom_na">
+            	<div class="bottom_nav_left2">
+                	<ul>
+                    	<li><a href="#">业内动态</a></li>
+                        <li><a href="#">业务视野</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="bottom_nav1">
+        <div class="bottom_nav2">
+        	<div class="bottom_title">
+            	人力资源
+            </div>
+            <div class="bottom_na">
+            	<div class="bottom_nav_left2">
+                	<ul>
+                    	<li><a href="#">人才理念</a></li>
+                        <li><a href="#">人才现状</a></li>
+                        <li><a href="#">人才政策</a></li>
+                        <li><a href="#">招聘信息</a></li>
+                        <li><a href="#">澄清公告</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <div class="bottom_right">
+        	<div class="bottom_logo">
+            	<img src="../../static/image/logo3.png"/>
+            </div>
+            <div class="main_search">
+            	<form action="#">
+                    <input class="main_search_input" type="text" value=""  name="keyword" placeholder="请输入关键字" />
+                    <input class="main_search_button" type="button" size="30" value="搜索" />
+                    <div class="cl"></div>
+                </form>
+            </div>
+            <div class="main_search2">
+            	<select class="main_select">
+                	<option>子站导航</option>
+                    <option>子站导航</option>
+                    <option>子站导航</option>
+                </select>
+            </div>
+            <div class="main_info">
+            	<p>地址：北京市朝阳区北三环东路8号静安中心3层</p>
+                <p>电话：010-64689035&nbsp;&nbsp;传真：010-64689030</p>
+                <p>E-mail:securities@capitalwater.cn</p>
+            </div>
         </div>
+        <div class="cl"></div>
     </div>
 </body>
 </html>
