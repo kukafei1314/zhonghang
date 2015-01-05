380f7107c4b8c4362948e492eca711b26226ec22
Mld <463471808@qq.com>
2015-01-05 19:47:20 +0800

    jstojquery

--- static/js/pic.js
+++ static/js/pic.js
@@ -11,9 +11,9 @@ function change(id){
 	clearInterval(scrollmove);
 	for (var i = 1; i <= 4; i++) {
 		if(i == id){
-			document.getElementById("smallimg_"+i).className="current";
+			$("#smallimg_"+i).attr("class","current");
 		}else{
-			document.getElementById("smallimg_"+i).className="";
+			$("#smallimg_"+i).attr("class","");
 		}
 	}
 	if ((next = id + 1) > 4){
@@ -25,14 +25,15 @@ function change(id){
 
 }
 function scrollMove(m){
-	var p = document.getElementById("pic").getElementsByTagName("li");
-	var srl = document.getElementById("pic").scrollLeft;
-	var dsrl = Math.floor((p[0].clientWidth*(m-1)-srl)/5);
-	var xsrl = Math.ceil((p[0].clientWidth*(m-1)-srl)/5);
-	if(srl > p[0].clientWidth*(m-1)){
-		document.getElementById("pic").scrollLeft = srl + dsrl;
-	}else if(srl < p[0].clientWidth*(m-1)){
-		document.getElementById("pic").scrollLeft = srl + xsrl;
+	var p = $("#pic li");
+	alert(p.width());
+	var srl = $("#pic").scrollLeft();
+	var dsrl = Math.floor((p.width()*(m-1)-srl)/5);
+	var xsrl = Math.ceil((p.width()*(m-1)-srl)/5);
+	if(srl > p.width()*(m-1)){
+		$("#pic").scrollLeft(srl + dsrl);
+	}else if(srl < p.width()*(m-1)){
+		$("#pic").scrollLeft(srl + xsrl);
 	}else{
 		clearInterval(scrollmove);
 	}
