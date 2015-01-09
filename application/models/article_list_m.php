82323b8472345a93b4de6497b0e95d597abe5b06
lzz <lzz19930210@163.com>
2015-01-09 17:25:39 +0800

    页码

--- application/models/article_list_m.php
+++ application/models/article_list_m.php
@@ -8,11 +8,12 @@ class Article_list_m extends CI_Model
    {
        parent::__construct();
        $this->load->database();
+       $this->load->library('pagination');
    }
    
-   public function query_article($pid,$tid)
+   public function query_article($pid,$tid,$start_page)
    {
-       $sql = "SELECT * FROM `zh_articles` WHERE `type` = $tid";
+       $sql = "SELECT * FROM `zh_articles` WHERE `type` = $tid ORDER BY `aid` ASC LIMIT $start_page , 10";
        $query = $this->db->query($sql);
        return $query->result_array();
    }
@@ -42,5 +43,32 @@ class Article_list_m extends CI_Model
    {
        $this->db->where('aid',$article['aid']);
        $this->db->update('zh_articles',$article);
+   }
+   
+   public function search_article($type,$search)
+   {
+       $sql = "SELECT * FROM `zh_articles` WHERE `type` = $type AND `title` LIKE '%".$search."%' ";
+       $query = $this->db->query($sql);
+       return $query->result_array();
+   }
+   
+   public function num_articles($type)
+   {
+       $sql = "SELECT COUNT(*) FROM `zh_articles` WHERE `type` = $type";
+       $query = $this->db->query($sql);
+       return $query->row_array()['COUNT(*)'];
+   }
+   
+   public function pageConfig($temp)
+   {
+       $config['base_url'] = $temp['base_url'];
+       $config['total_rows'] = $temp['total_rows'];
+       $config['per_page'] = 10;
+       $config['first_link'] = "首页";
+       $config['last_link'] = "末页";
+       $config['use_page_numbers'] = TRUE;
+       $config['cur_tag_open'] = '<b>';
+       $config['cur_tag_close'] = '</b>';
+       $this->pagination->initialize($config);
    }
 }
