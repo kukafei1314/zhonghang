04227ac8ca712f4182afd8c483106b8759cb1cf1
lzz <lzz19930210@163.com>
2015-01-11 09:50:16 +0800

    一级标题

--- application/views/admin/admin_header.php
+++ application/views/admin/admin_header.php
@@ -110,7 +110,7 @@
 				</ul>
 			</li>
 			<li class="parent" id="tables">
-				<a id="parent_2" onclick="parent_hold(2)">
+				<a id="parent_2" onclick="parent_hold(2)" href="<?php echo base_url('admin/subtitle/listNews?pid=0&tid=4');?>" target="main">
 					<span class="glyphicon glyphicon-eye-close"></span> 研发中心
 				</a>
 			</li>
@@ -150,21 +150,23 @@
 				</ul>
 			</li>
 			<li class="parent" id="tables">
-				<a id="parent_3" onclick="parent_hold(3)" >
+				<a id="parent_3" onclick="parent_hold(3)" href="<?php echo base_url('admin/subtitle/listNews?pid=0&tid=6');?>" target="main">
 					<span class="glyphicon glyphicon-download-alt"></span> 下载中心
 				</a>
 			</li>
 			<li class="parent" id="tables">
-				<a id="parent_4" onclick="parent_hold(4)" >
+				<a id="parent_4" onclick="parent_hold(4)" href="<?php echo base_url('admin/subtitle/listNews?pid=0&tid=7');?>" target="main">
 					<span class="glyphicon glyphicon-comment"></span> 公司公告
 				</a>
 			</li>
 			<li role="presentation" class="divider"></li>
-			<li>
+<!--		
+            <li>
 				<a href="<?php echo base_url('admin/article_type');?>" id="index" target="main">
 					<span class="glyphicon glyphicon-th-large"></span> 类别管理
 				</a>
 			</li>
+-->
 		</ul>
 	</div><!--/.sidebar-->
     <div id="mainframe">
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -22,7 +22,7 @@
 	
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-			<div class="login-panel panel panel-default" >
+			<div class="login-panel panel panel-default my_login" >
 				<div class="panel-heading" style = "text-align:center; font-family:微软雅黑; font-size:25px;">管理员登录</div>
 				<div class="panel-body">
 							
