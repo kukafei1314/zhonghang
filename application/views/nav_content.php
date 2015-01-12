5a78f8acf402d4ed58708379b0a4f7413e1ca089
lizzy <lizzy.999@qq.com>
2015-01-12 23:06:18 +0800

    改三级页面

--- application/views/nav_content.php
+++ application/views/nav_content.php
@@ -5,7 +5,7 @@
         <div id="right_content_detail">
            <div class="article_title"><?php echo $article['title'];?></div> 
 		   <div class="article_time">发布时间：<?php echo date("Y-m-d",$article['add_time']);?></div> 
-           <p><?php echo $article['content'];?></p>
+           <div class="article_content"><?php echo $article['content'];?></div>
         </div>
     </div>
 	<div class="clear"></div>
