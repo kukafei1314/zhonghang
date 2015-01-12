d46344728aa37dd1e83a03c23659a492124903ce
lizzy <lizzy.999@qq.com>
2015-01-12 23:10:59 +0800

    三级页面修改

--- application/views/nav_content_about.php
+++ application/views/nav_content_about.php
@@ -3,8 +3,8 @@
         	<p><a href="<?php echo base_url('index');?>">首页</a>&nbsp;><a href=""><?php echo $title;?></a>&nbsp;><a href=""><?php echo $title2;?></a></p>
         </div>
         <div id="right_content_detail">
-        	<h2 align="center"><?php echo $title;?></h2> 
-			<?php echo $content;?>
+        	<div class="article_title"><?php echo $title2;?></div> 
+			<div class="article_content"><?php echo $content;?></div>
         </div>
     </div>
 	<div class="clear"></div>
