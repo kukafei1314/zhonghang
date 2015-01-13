8aebaa959e6e81fc64a3760b463e0adb9f18a12c
lisen9129@163.com <lisen9129@163.com>
2015-01-13 21:09:41 +0800

    Jason改了一个输出符号

--- application/views/navigation_about.php
+++ application/views/navigation_about.php
@@ -4,9 +4,11 @@
 				<div class="naviTitleCh"><?php echo $title; ?></div>
 				<div class="naviTitleEn"><?php echo $en_title; ?></div>
 			</div>
+			
 			<div class="leftNaviMenu">
+				
 				<?php foreach ($title_second as $title):?>
-					<div class="naviMenuButton"><a href="<?php echo base_url('navigcontrol/?tid=').$title['tid'];?>"> <?=$title['title']?></a></div>
+					<div class="naviMenuButton"><a href="<?php echo base_url('navigcontrol/?tid=').$title['tid'];?>"> <?php echo $title['title'];?></a></div>
 				<?php endforeach;?>
 			</div>
 		</div>
