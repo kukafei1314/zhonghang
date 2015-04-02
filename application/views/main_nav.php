4681438bc56047e7b72b93a46c919c3a594a033c
Mld <463471808@qq.com>
2015-04-02 22:40:11 +0800

    test

--- application/views/main_nav.php
+++ application/views/main_nav.php
@@ -2,12 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml"/>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet"  media="screen and (min-width:900px)" href="<?php echo base_url('static/css/home.css'); ?>"/>
-<link rel="stylesheet"  media="screen and (min-width:900px)" href="<?php echo base_url('static/css/home_middle.css'); ?>"/>
 <link rel="stylesheet"  media="screen and (min-device-width:900px)" href="<?php echo base_url('static/css/home.css'); ?>"/>
 <link rel="stylesheet"  media="screen and (min-device-width:900px)" href="<?php echo base_url('static/css/home_middle.css'); ?>"/>
 <link rel="stylesheet" media="screen and (max-device-width:900px)" href="<?php echo base_url('static/css/small.css'); ?>" type="text/css" />
-<link rel="stylesheet" media="screen and (max-width:900px)" href="<?php echo base_url('static/css/small.css'); ?>" type="text/css" />
 <script type="text/javascript" src="<?php echo base_url('static/js/jquery-1.8.0.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('static/js/pic.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('static/js/home.js'); ?>"></script>
