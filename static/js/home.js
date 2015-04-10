178df43c240a8260bf4023216ecbc31f9e730741
Mld <463471808@qq.com>
2015-04-10 10:38:12 +0800

    导航

--- static/js/home.js
+++ static/js/home.js
@@ -48,8 +48,16 @@ $(document).ready(function() {
 	}
 	function touchMove(event) {
 		var touch = event.originalEvent.targetTouches[0];
-		  tempX = tempX + touch.pageX - mouseDownPosiX;
-		  tempY = tempY + touch.pageY - mouseDownPosiY;
+		tempX = tempX + touch.pageX - mouseDownPosiX;
+		tempY = tempY + touch.pageY - mouseDownPosiY;
+		if(tempX < 200 && Math.abs(tempY)<10 && device_width<900) {
+			$(".leftNavi").animate({left: '+'+(tempX-200)+'px'}, "fast");
+			$(".list_right").animate({left: '+'+tempX+'px'}, "fast");
+			$(".top_second").animate({left: '+'+tempX+'px'}, "fast");
+			$("#bottom_div").animate({left: '+'+tempX+'px'}, "fast");
+		}
+	 }
+	 function touchEnd() {
 		  if(tempX > 40 && Math.abs(tempY)<10 && device_width<900) {
 				$(".leftNavi").animate({left: '+0px'}, "fast");
 				$(".list_right").animate({left: '+200px'}, "fast");
@@ -61,8 +69,6 @@ $(document).ready(function() {
 				$(".top_second").animate({left: '0px'}, "fast");
 				$("#bottom_div").animate({left: '0px'}, "fast");
 		  }
-	 }
-	 function touchEnd() {
 		  tempX = 0;
 		  tempY = 0;
 	}
