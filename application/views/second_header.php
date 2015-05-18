f3037073cca81cd2d6d56c02f283df47d5eeef1e
Mld <463471808@qq.com>
2015-05-18 16:40:46 +0800

    小改

--- application/views/second_header.php
+++ application/views/second_header.php
@@ -51,5 +51,5 @@
             </div>
             <div class="cl"></div>
 		</div>
-        <div style="background-image:url(../../static/image/top_second.png); height:280px; width:100%"></div>
+        <div style="background:url(../../static/image/top_second.png) no-repeat center center; height:280px;"></div>
 	</div>
